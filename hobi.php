7cc6ce2a82ee2cb84ebc0a05d4e38d6297b487c2
Alvi fauzan al farizi <alvi.fauzanalvarizi008@gmail.com>
2026-08-14 09:41:57 +0700

    Add files via upload

---
+++ hobi.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesh;eet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<style>
+.opening{
+    background-color:rgb(30,82,253);
+    padding:15px;
+    margin:30px;
+    border-radius:30px;
+    font-family: Arial,Helvetica, sans-serif;
+    margin-top: 20px;
+}
+</style>
+
+
+</head>
+<body>
+    <div class="opening">
+        <h1><center>WELCOME TO MY WEB PERSONAL</center></h1>
+    </div>
+
+     <div class="container text-center">
+    <div class="row row-cols-5">
+        <a href="index2.php" class="btn btn-primary">home</a>
+
+        <a href="biodata.php" class="btn btn-primary">biodata</a>
+        <a href="hobi.php" class="btn btn-primary">hobi</a>
+        <a href="aboutme.php" class="btn btn-primary">about me</a>
+    </div>
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div><h1> my profile</h1></div>
+           
+        <div><nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="biodata.php">biodata</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="hobi.php">hobi</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="aboutme.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      about me
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Action</a></li>
+                      <li><a class="dropdown-item" href="#">Another action</a></li>
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                  </li>
+                </ul>
+                <form class="d-flex" role="search">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+              </div>ini login</div>
+            </div>
+          </nav>
+        </div>
+
+            
+
+          <div class="card" style="width: 18rem;">
+  <img src="skylar.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">skylar</h5>
+    <p class="card-text">atlet profesional Mobile Legends: Bang Bang asal Indonesia yang lahir di Manado pada 25 Februari 2002. Ia dikenal luas sebagai mantan pemain kunci RRQ Hoshi sebelum pindah dan bergabung dengan ONIC pada Agustus 2025.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+
+  <div class="card" style="width: 18rem;">
+  <img src="claude.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">claude</h5>
+    <p class="card-text">Claude adalah pencuri hebat dari Los Pecados yang menganggap aksi mencuri sebagai sebuah seni. Ia selalu memberi tahu korbannya sebelum beraksi dan ditemani oleh seekor monyet pintar bernama Dexter.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>  
+</body>
+</html>
